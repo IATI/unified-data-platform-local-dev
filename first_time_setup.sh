cb1500c6c4f887126292240e7f77a5f2ae95b9db
Simon K <6615834+simon-20@users.noreply.github.com>
2025-06-24 15:56:54 +0100

    build: Azurite ports, queue, Solr ver
    * Change Azurite ports so this can be run more easily alongside other instances of Azurite
    * Create the storage queue used for flagging reporting orgs
    * Explicitly set Solr version due to incompatibilities of Solr configs with older versions.

--- first_time_setup.sh
+++ first_time_setup.sh
@@ -2,7 +2,7 @@
 
 # this command should be run after 'docker compose up' is working
 
-export AZURE_STORAGE_CONNECTION_STRING="DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://localhost:10000/devstoreaccount1;QueueEndpoint=http://localhost:10001/devstoreaccount1;TableEndpoint=http://localhost:10002/devstoreaccount1;"
+export AZURE_STORAGE_CONNECTION_STRING="DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://localhost:13000/devstoreaccount1;QueueEndpoint=http://localhost:13001/devstoreaccount1;TableEndpoint=http://localhost:13002/devstoreaccount1;"
 
 echo "Running storage container create --name source"
 az storage container create --name source
@@ -15,6 +15,10 @@ az storage container create --name lake
 
 echo "Running az storage container create --name validator-adhoc"
 az storage container create --name validator-adhoc
+
+echo "Running az storage queue create --name publisher-black-flag-remove"
+az storage queue create --name publisher-black-flag-remove
+
 
 echo "Creating Solr cores for activities, transactions, and budgets"
 docker compose exec  -it  iati-refresher-solr  solr create_core -c activity_solrize -d /datastore-solr-configs/configsets/activity/conf
